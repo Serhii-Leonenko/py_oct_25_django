4742282472d0914c7911c4ecd96a4dfaef775ae4
Serhii-Leonenko <leonenko3149@gmail.com>
2025-12-19 11:36:04 +0200

    added signals

--- users/models.py
+++ users/models.py
@@ -1,5 +1,21 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="profile"
+    )
+    bio = models.TextField(
+        blank=True, null=True
+    )
+
+    def __str__(self):
+        return self.user
